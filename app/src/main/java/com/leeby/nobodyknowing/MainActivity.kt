30bf35e8d0b556ef8bb232421cce72738b409ce1
makehoney <makehoney3@gmail.com>
2020-11-30 02:51:39 +0900

    알림 권한 요청 intent 추가

--- app/src/main/java/com/leeby/nobodyknowing/MainActivity.kt
+++ app/src/main/java/com/leeby/nobodyknowing/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.leeby.nobodyknowing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isNotificationPermissionAllowed()) {
+            // notification 퍼미션 권한 요구 액티비티 시작
+            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
+            startActivity(intent)
+        }
+    }
+
+    private fun isNotificationPermissionAllowed(): Boolean {
+        val notificationListenerSet = NotificationManagerCompat.getEnabledListenerPackages(this)
+        val myPackageName = packageName
+
+        return notificationListenerSet.contains(myPackageName)
     }
 }
